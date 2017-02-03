97c5c409f9d3dc933ca65f8e7a238f4ce2ab3c4b
Danielle H <daheber@gmail.com>
2017-02-02 19:28:57 -0800

    logging and export routeList

---
+++ TODO.md
@@ -0,0 +1,15 @@
+TODO:
+
+- connection to a database (probably MongoDB)
+- ADD ROUTES
+  GET /
+  GET /:id
+  POST /
+  PUT /:id
+  DELETE /:id
+- ADD OPTIONS for routes
+  params 
+  optional params
+  query string
+- Serve static files
+- Testing
--- server.js
+++ server.js
@@ -1,51 +1,39 @@
 const Hapi = require('hapi');
+const routeList = require('./lib/routes/crud.js');
 
 //create server
 const server = new Hapi.Server();
+
 server.connection({
   host: 'localhost',
   port: 8000
 });
 
-//add routes
-const routeList = [
-  {
-    method: 'GET',
-    path: '/hello/{name?}',
-    handler: (req, res) => {
-      if(req.params.name) return res(`Hi ${req.params.name}`);
-      return res(`Hey stranger`);
+server.register([
+    {
+      register: require('good'),
+      options: {
+        ops: {
+          interval: 1000
+        },
+        reporters: {
+          console: [
+            {
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ log: '*', response: '*', request: '*' }]
+            },
+            {
+              module: 'good-console'
+            }, 
+            'stdout'
+            ]
+        }
+      }
     }
-  },
-  {
-    method: 'GET',
-    path: '/bye/{name?}',
-    handler: (req, res) => {
-      if(req.params.name) return res(`Bye ${req.params.name}`);
-      return res(`Bye stranger`);
-    }
-  },
-  {
-    method: 'GET',
-    path: '/query',
-    handler: (req, res) => {
-      console.log(req.query);
-      if(req.query.id) return res(`query string is ${req.query.id}`);
-      return res(`No query string recreived`);
-    },
-    config: {
-      description: 'Query string test',
-      notes: 'If there\'s an id query it will print it out'
-    }
-  }
-];
-// server.route({
-//   method: 'GET',
-//   path: '/hello',
-//   handler: (req, res) => {
-//     return res('Hello World!');
-//   }
-// });
+], err => {
+  if (err) return console.error(err);
+});
 
 server.route(routeList);
 
